21898a70d235965cd1163bbf52262da347ef6141
braydencw1 <braydencw1@users.noreply.github.com>
2023-11-15 15:07:49 -0600

    Moved functions to organized files

---
+++ cmd/venova/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"venova/bot"
+	"venova/db"
+
+	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	// Load environment variables from the .env file
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file:", err)
+	}
+	token := os.Getenv("TOKEN")
+
+	// Initialize Discord session
+	discord, err := discordgo.New("Bot " + token)
+	if err != nil {
+		log.Fatal("Error creating Discord session:", err)
+	}
+	discord.Identify.Intents = discordgo.IntentsGuilds | discordgo.IntentsGuildMessages | discordgo.IntentsGuildVoiceStates
+
+	// Open a connection to Discord
+	if err := discord.Open(); err != nil {
+		log.Fatal("Error opening Discord connection:", err)
+	}
+	defer discord.Close()
+	discord.AddHandler(bot.OnReady)
+	// Register the messageCreate functfunc messageCreate(s *discordgo.Session, m *discordgo.MessageCreate)ion as a callback for the MessageCreate event
+	// discord.AddMessageCreateHandler(messageCreate)
+	discord.AddHandler(bot.HandleMessageEvents)
+	discord.AddHandler(bot.HandleVoiceStateUpdate)
+	// Keep the bot running
+	log.Println("Bot is now running. Press Ctrl+C to exit.")
+	dbUsername := os.Getenv("DB_USER")
+	dbHost := os.Getenv("DB_HOST")
+	dbDB := os.Getenv("DB_DB")
+	dbPassword := os.Getenv("DB_PASS")
+	dbPort := os.Getenv("DB_PORT")
+	dsn := fmt.Sprintf("user=%s password=%s host=%s port=%s dbname=%s sslmode=disable", dbUsername, dbPassword, dbHost, dbPort, dbDB)
+	err = db.OpenDatabase(dsn)
+	if err != nil {
+		log.Panicf("Database connection is rough, to say the least: %v", err)
+	}
+	go bot.BirthdateCheckRoutine(discord)
+	select {} // Block the main goroutine indefinitely
+}
