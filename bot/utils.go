732af42980fcb7ef91531ef12e8599a07dbb9301
braydencw1 <50538489+braydencw1@users.noreply.github.com>
2025-06-01 19:03:27 -0500

    fix default

--- bot/utils.go
+++ bot/utils.go
@@ -41,6 +41,7 @@ func GetEnvOrDefault(key, def string) string {
 	v := os.Getenv(key)
 	if v == "" {
 		log.Printf("%s is empty or not defined. Defaulting to: %s", key, def)
+		v = def
 	}
-	return def
+	return v
 }
